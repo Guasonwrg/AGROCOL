4632e061ba8dfb3c69a7b7792f60c2906db3d4f2
Guasonwrg <willy.romero@estudiantes.utec.edu.uy>
2022-10-14 18:55:31 -0300

    Initial commit

---
+++ src/test/java/com/agrocol/agrocol/AgrocolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrocol.agrocol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrocolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
